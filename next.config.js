4640ac43db1092e85a96feb45fcabd0881d6f184
NE-SmallTown <18418010+NE-SmallTown@users.noreply.github.com>
2022-01-25 21:40:31 +0800

    Chore: deploy Github Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   future: {
     webpack5: true,
   },
+  basePath: '/redux-vs-mobx',
+  assetPrefix: '/redux-vs-mobx/'
 };
